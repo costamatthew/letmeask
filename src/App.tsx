9bcfd659a45e9404a58a26367a6c03c9b156f7b4
Mateus Costa <mateusdsilvacosta@outlook.com>
2021-07-10 23:14:07 -0300

    Creating Auth Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
 import { Home } from "./Pages/Home";
+import { NewRoom } from "./Pages/NewRoom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
